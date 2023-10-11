d565760098fc03b148874a702f65615953d582e8
Shubham Khatri <shubham.khatri08@gmail.com>
2023-10-11 17:33:53 +0530

    order, payment, address, bank details doctypes added

---
+++ vendor_app/vendor_app/doctype/supplier_bank_details/supplier_bank_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Trampoline and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Supplier Bank Details", {
+// 	refresh(frm) {
+
+// 	},
+// });
